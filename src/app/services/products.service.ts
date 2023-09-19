2ff6c274c6604dfc4b54042b29a02fd58e737201
iryna-zatynina <ira.zat1997@gmail.com>
2023-09-19 15:43:20 +0200

    add error message

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,13 +1,17 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {catchError, delay, Observable, retry, throwError} from "rxjs";
 import {IProduct} from "../models/product";
+import {ErrorService} from "./error.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private errorService: ErrorService
+  ) {
   }
 
   getAll() : Observable<IProduct[]> {
@@ -15,6 +19,15 @@ export class ProductsService {
       params: new HttpParams({
         fromObject: {limit: 5}
       })
-    })
+    }).pipe(
+      delay(1000),
+      retry(2),
+      catchError(this.errorHandler.bind(this))
+    )
+  }
+
+  private  errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message)
+    return throwError(() => error.message)
   }
 }
