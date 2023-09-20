099a128cdc232c2e38de0d399c2d9a4bf8165b6d
iryna-zatynina <ira.zat1997@gmail.com>
2023-09-20 15:08:19 +0200

    add form & validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import {ProductComponent} from "./components/product/product.component";
 import {HttpClientModule} from "@angular/common/http";
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { FilterProductsPipe } from './pipes/filter-products.pipe';
 import { ModalComponent } from './components/modal/modal.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,15 @@ import { ModalComponent } from './components/modal/modal.component';
     ProductComponent,
     GlobalErrorComponent,
     FilterProductsPipe,
-    ModalComponent
+    ModalComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
 
   providers: [],
