f396c862b4b96d2f0cb7f1d6716e8a07257f08fb
iryna-zatynina <ira.zat1997@gmail.com>
2023-09-20 15:00:48 +0200

    add modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,23 @@ import { AppComponent } from './app.component';
 import {ProductComponent} from "./components/product/product.component";
 import {HttpClientModule} from "@angular/common/http";
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
-import {FormsModule} from "@angular/forms";
 import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     GlobalErrorComponent,
-    FilterProductsPipe
+    FilterProductsPipe,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    HttpClientModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
